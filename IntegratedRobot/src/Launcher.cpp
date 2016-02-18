0c117a7e124fba1aa7514dcbfc0f8a62f0577c26
Joseph Gelfand <35927@psdschools.org>
2016-02-17 18:01:27 -0700

    Changed Aim() to the bugfixed/modified version from flywheel-test
    ->This was the only change, but it includes a shocking amount of
    bugfixes. if you revert this, make sure to fix those again

--- IntegratedRobot/src/Launcher.cpp
+++ IntegratedRobot/src/Launcher.cpp
@@ -48,14 +48,16 @@ void Launcher::Aim(float range)//takes horizontal range, in meters
 {
 	float term = sqrt(2*.295*9.81/(.5*1.001*.0507));//2* boulder weight, g, drag, air density, cross-sectional area repectively
 	float dH= 2.77-.254;//delta height
-	float Vv0=sqrt(pow(term, 2)*(pow(.577215, dH*2*9.81/pow(term,2))-1));//.57... is euler's constant
+	float Vv0=sqrt(pow(term, 2)*(pow(2.71828, dH*2*9.81/pow(term,2))-1));//2.7... is euler's constant
 	float Tvmax= (term/9.81)*atan(Vv0/term);//time to vmax
-	float Vh0=pow(term,2)*(pow(.577215, 9.81*range/pow(term,2))-1)/(9.81*Tvmax);//initial Horiz. Vel.
-	SetAngle(atan((Vv0/Vh0)*(3.1415/180)));//calculate and set target angle
+	float Vh0=pow(term,2)*(pow(2.71828, 9.81*range/pow(term,2))-1)/(9.81*Tvmax);//initial Horiz. Vel.
+	SetAngle(atan(Vv0/Vh0)*(180/3.1415));//calculate and set target angle
 	float V0= sqrt(pow(Vh0,2)+pow(Vv0, 2));
 	float r=.058;
-	float Iwheel=19.42;
-	float targetSpeed= r*V0*sqrt(1+(.295/Iwheel));//target speed in m
-	targetSpeed*=7500;//convert from meters to ticks
+	float rball = .127;
+	float Iwheel=19.42;//the .168 below is ~ distance from flywheel center to center of shooter
+	float targetSpeed= V0*((((.168)/Iwheel)*(.295/2))+(1/r));//target speed in rad/s
+	targetSpeed*=900/(2*3.1415);//convert from radians to ticks
 	SetTargetSpeed(targetSpeed);
+
 }
